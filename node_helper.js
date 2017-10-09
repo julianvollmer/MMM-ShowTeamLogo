b16cfa365c179894263b55d0cb3dcaab004a6d0a
Julian Vollmer <>
2017-10-09 12:08:30 +0200

    implement callback

--- node_helper.js
+++ node_helper.js
@@ -9,7 +9,7 @@ module.exports = NodeHelper.create({
 		
 		if(notification === "CONNECTED"){
 			console.log(this.name + " ************* received a socket notification: " + notification + " - Payload: " + JSON.stringify(payload, null, 4 ));
-			fball = new Football(payload)
+			fball = new Football(payload, this);
 		}
 
 		if (notification === "LOG"){
@@ -25,17 +25,19 @@ module.exports = NodeHelper.create({
 
     start: function() {        
         this.sendSocketNotification("CONNECTED", "connected");
-        this.update();
     },
 
     update: function () {
     	var self = this;
-        self.sendSocketNotification("UPDATEUI", "options");
-        setTimeout(function() {
-        	var table = fball.leagueTable.getTable();
-        	table.sort(fball.helper.sortBy('position', false, parseInt));
-        	self.sendSocketNotification("GET_URL_PATH_TO_LOGO", fball.team.getUrlToLogo());
-        }, 20000);
+        self.sendSocketNotification("UPDATEUI", "options");        
+		var table = fball.leagueTable.getTable();
+    	table.sort(fball.helper.sortBy('position', false, parseInt));
+    	self.sendSocketNotification("GET_URL_PATH_TO_LOGO", fball.team.getUrlToLogo());
+        
+    }, 
+
+    callbackFromFootballApi: function function_name(argument) {
+    	this.update();
     }
 });
 
